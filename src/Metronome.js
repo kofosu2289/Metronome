8970325644044c415a7bbe49274a52e6983b638b
Sarah Duncan <SDuncan@locus.co>
2019-01-11 16:50:20 -0500

    add click sounds to start/stop button

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,34 +1,46 @@
 import React, { Component } from 'react';
 import './Metronome.css';
+import click1 from './click1.wav';
+import click2 from './click2.wav';
 
 class Metronome extends Component {
-		constructor(props) {
-				super(props);
+  constructor(props) {
+    super(props);
 				
-				this.state = {
-						playing: false,
-						count: 0,
-						bpm: 100,
-						beatsPerMeasure: 4
-				};
-		}
+		this.state = {
+		  playing: false,
+		  count: 0,
+		  bpm: 100,
+		  beatsPerMeasure: 4
+		};
+    
+    this.click1 = new Audio(click1);
+    this.click2 = new Audio(click2);
+  }
 		
-		handleBpmChange = event => {
-				const bpm = event.target.value;
-				this.setState({bpm});
-		}
+  handleBpmChange = event => {
+    const bpm = event.target.value;
+    this.setState({bpm});
+  }
+  
+  startStop = () => {
+    this.click1.play();
+  }
 		
   render() {
     const { playing, bpm } = this.state;
 			
     return(
-    	<div className = "metronome">
-				<div className = "bpm-slider">
-					<div> {bpm} BPM</div>
-						<input type = "range" min = "60" max = "240" value = {bpm}
-						onChange={this.handleBpmChange} />
-					</div>
-					<button> {playing ? 'Stop' : 'Start'}</button>
+      <div className = "metronome">
+		    <div className = "bpm-slider">
+				  <div> {bpm} BPM</div>
+				    <input type = "range" 
+                    min = "60" 
+                    max = "240" 
+                    value = {bpm} 
+                    onChange={this.handleBpmChange} />
+				  </div>
+					<button onClick = {this.startStop}> {playing ? 'Stop' : 'Start'}</button>
 			</div>
 		);
   }
